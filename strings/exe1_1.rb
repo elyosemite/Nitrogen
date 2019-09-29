e393dd9b54c2fac1d05b5bdd549ab61bda271ee6
yurism <yuri.ifal@gmail.com>
2019-09-29 20:23:30 -0300

    More code

--- strings/exe1_1.rb
+++ strings/exe1_1.rb
@@ -1,6 +1,8 @@
 # Problem
 # You want to iterate over a data structure, building a string it as you do.
 
+
+# Part I
 hash = { "key1" => "val1", 
 	     "key2" => "val2",
 	     "key3" => "val3",
@@ -13,3 +15,23 @@ hash = { "key1" => "val1",
 string = ""
 hash.each { |k,v| string << "#{k} is #{v}\n" }
 puts string
+puts "\n\n"
+
+# Part II - with symbols
+person = { firstName: :yuri, lastName: :melo, address: :anyone}
+str_concat = ""
+person.each { |k,v| str_concat << k.to_s << " ----- " << v.to_s << "\n" }
+puts str_concat
+puts "\n\n"
+
+
+# Part III
+# This varaint of the simple solution is slightly more efficient, but rather to read
+string2 = ""
+hash.each { |k,v| string2 << k << " is " << v << "\n" }
+puts string2
+puts "\n\n"
+
+# Part IV
+
+puts hash.keys.join("\n") + "\n"
