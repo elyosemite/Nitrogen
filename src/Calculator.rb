4a7614d7f5136da278558e4abcfbfc0de9440560
Yuri Melo <yurifullstack@gmail.com>
2023-05-06 00:53:43 -0300

    Add Unit Tests

--- src/Calculator.rb
+++ src/Calculator.rb
@@ -1,62 +1,40 @@
 require 'securerandom'
 
-require 'CarIndustry'
-
 # This is Calculator
+module Calc
+	class Calculator
+		attr_reader :title, :author
+		attr_writer :date
+		attr_accessor :description
 
-class Book
-	attr_reader :title, :author
-	attr_writer :date
-	attr_accessor :description
+		def initialize()
+			@title = nil
+			@author = nil
+			@date = "2023-05-05T14:49:44"
+			@description = "Calculing"
+			@identifier = SecureRandom.uuid
+		end
 
-	def initialize(title, author)
-		@title = title
-		@author = author
-		@date = "2023-05-05T14:49:44"
-		@description = "Nothing"
-		@identifier = SecureRandom.uuid
-	end
+		def who_am_i
+			puts @identifier
+			puts "I am Harry Potter"
+		end
 
-	def who_am_i
-		puts @identifier
-		puts "I am Harry Potter"
-	end
+		def self.Description
+			puts "I am Calculate for Engineers"
+			puts @description
+		end
 
-	def self.Description
-		puts "I am a Class Method"
-		puts @description
+		def sum(a, b)
+			return a + b
+		end
+
+		def sub(a, b)
+			return a - b
+		end
+
+		def mult(a, b)
+			return a * b
+		end
 	end
 end
-
-class Car < CarIndustry::Vehicle
-	def CarType
-		puts "Small Car"
-	end
-end
-
-class Truck < CarIndustry::Vehicle
-	def CarType
-		puts "Big Car"
-	end
-end
-
-
-# Aqui é onde o Acoplamento Mora e Reina rs
-hp = Book.new("This is a book", "Harry Potter")
-hp.who_am_i
-
-puts hp.title
-puts hp.author
-hp.description = "One description about Harry Potter"
-puts hp.description
-Book.Description
-puts "\n\n"
-
-v = CarIndustry::Vehicle.new
-v.CarType
-
-car = Car.new
-car.CarType
-
-truck = Truck.new
-truck.CarType
