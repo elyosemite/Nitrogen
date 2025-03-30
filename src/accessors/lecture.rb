306a4b63548acb04c7fa6b9ace410ff8520b4228
Yuri Melo <11520745+elyosemite@users.noreply.github.com>
2025-03-30 19:29:05 -0300

    Add simple Adjustment by using neovim

--- src/accessors/lecture.rb
+++ src/accessors/lecture.rb
@@ -2,10 +2,13 @@ require 'byebug'
 
 class FalconHeavy
 
-    attr_accessor :buildModule01, :buildModule02, :buildModule03, :buildModule04
+  attr_accessor :buildModule01, :buildModule02, :buildModule03, :buildModule04
 
-    def initialize(name = "Falcon")
-        @name = name
-    end
-    
+  def initialize(name = "Falcon")
+    @name = name
+  end
+
+  def show;
+  end
+
 end
